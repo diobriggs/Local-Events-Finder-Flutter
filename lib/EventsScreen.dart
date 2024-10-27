c79362ee526613c1778db47432ee43883bcae2bf
Dio Briggs <diobriggs@icloud.com>
2024-10-26 20:20:03 -0400

    Added more events for each city

--- lib/EventsScreen.dart
+++ lib/EventsScreen.dart
@@ -12,6 +12,148 @@ class EventsScreen extends StatelessWidget {
       {'name': 'Food Truck Event', 'date': '2024-11-12', 'time': '12:00 PM', 'price': 'Free'},
       {'name': 'Art Exhibit', 'date': '2024-11-15', 'time': '10:00 AM', 'price': '\$30'}
     ],
+    'New York, NY': [
+      {'name': 'Broadway Show', 'date': '2024-11-10', 'time': '7:00 PM', 'price': '\$120'},
+      {'name': 'Central Park Concert', 'date': '2024-11-12', 'time': '6:00 PM', 'price': 'Free'},
+      {'name': 'Museum Night', 'date': '2024-11-15', 'time': '8:00 PM', 'price': '\$25'}
+    ],
+    'Los Angeles, CA': [
+      {'name': 'Hollywood Movie Premiere', 'date': '2024-11-11', 'time': '8:00 PM', 'price': '\$75'},
+      {'name': 'Foodie Fest', 'date': '2024-11-13', 'time': '11:00 AM', 'price': 'Free'},
+      {'name': 'Sunset Art Walk', 'date': '2024-11-16', 'time': '4:00 PM', 'price': '\$20'}
+    ],
+    'Chicago, IL': [
+      {'name': 'Jazz Concert', 'date': '2024-11-10', 'time': '6:30 PM', 'price': '\$40'},
+      {'name': 'River Food Tour', 'date': '2024-11-14', 'time': '2:00 PM', 'price': '\$30'},
+      {'name': 'Architecture Boat Tour', 'date': '2024-11-17', 'time': '10:00 AM', 'price': '\$45'}
+    ],
+    'Houston, TX': [
+      {'name': 'Rodeo Festival', 'date': '2024-11-09', 'time': '3:00 PM', 'price': '\$20'},
+      {'name': 'BBQ Cook-off', 'date': '2024-11-13', 'time': '1:00 PM', 'price': 'Free'},
+      {'name': 'Art in the Park', 'date': '2024-11-18', 'time': '11:00 AM', 'price': '\$15'}
+    ],
+    'Phoenix, AZ': [
+      {'name': 'Desert Music Festival', 'date': '2024-11-12', 'time': '5:00 PM', 'price': '\$60'},
+      {'name': 'Food Truck Fiesta', 'date': '2024-11-14', 'time': '12:00 PM', 'price': 'Free'},
+      {'name': 'Indigenous Art Fair', 'date': '2024-11-18', 'time': '9:00 AM', 'price': '\$25'}  
+    ],
+    'Philadelphia, PA': [
+      {'name': 'Liberty Jazz Night', 'date': '2024-11-10', 'time': '7:00 PM', 'price': '\$30'},
+      {'name': 'Historic Food Tasting', 'date': '2024-11-13', 'time': '1:00 PM', 'price': '\$20'},
+      {'name': 'Art Museum Tour', 'date': '2024-11-16', 'time': '11:00 AM', 'price': '\$25'}
+    ],
+    'San Antonio, TX': [
+      {'name': 'Fiesta at the Alamo', 'date': '2024-11-09', 'time': '5:00 PM', 'price': '\$20'},
+      {'name': 'Market Square Fest', 'date': '2024-11-12', 'time': '12:00 PM', 'price': 'Free'},
+      {'name': 'River Walk Lights', 'date': '2024-11-15', 'time': '6:00 PM', 'price': '\$10'}
+    ],
+    'San Diego, CA': [
+      {'name': 'Beach Concert', 'date': '2024-11-11', 'time': '4:00 PM', 'price': '\$45'},
+      {'name': 'Food & Wine Expo', 'date': '2024-11-13', 'time': '2:00 PM', 'price': '\$30'},
+      {'name': 'Art on the Pier', 'date': '2024-11-16', 'time': '10:00 AM', 'price': '\$25'}
+    ],
+    'Dallas, TX': [
+      {'name': 'Cowboy Music Fest', 'date': '2024-11-12', 'time': '7:00 PM', 'price': '\$50'},
+      {'name': 'Food Festival', 'date': '2024-11-14', 'time': '11:00 AM', 'price': 'Free'},
+      {'name': 'State Fair Rides', 'date': '2024-11-18', 'time': '12:00 PM', 'price': '\$15'}
+    ],
+    'Austin, TX': [
+      {'name': 'Live Music Night', 'date': '2024-11-10', 'time': '6:00 PM', 'price': '\$40'},
+      {'name': 'Street Food Bash', 'date': '2024-11-13', 'time': '5:00 PM', 'price': 'Free'},
+      {'name': 'Art Studio Tour', 'date': '2024-11-17', 'time': '9:00 AM', 'price': '\$10'}
+    ],
+    'Jacksonville, FL': [
+      {'name': 'Beach Festival', 'date': '2024-11-11', 'time': '5:00 PM', 'price': '\$25'},
+      {'name': 'Food Truck Rally', 'date': '2024-11-14', 'time': '1:00 PM', 'price': 'Free'},
+      {'name': 'Art Fair', 'date': '2024-11-17', 'time': '10:00 AM', 'price': '\$15'}    
+    ],
+    'Columbus, OH': [
+      {'name': 'Music Showcase', 'date': '2024-11-12', 'time': '7:00 PM', 'price': '\$30'},
+      {'name': 'Food Fest', 'date': '2024-11-15', 'time': '2:00 PM', 'price': 'Free'},
+      {'name': 'Craft Market', 'date': '2024-11-18', 'time': '11:00 AM', 'price': '\$10'}
+    ],
+    'Indianapolis, IN': [
+      {'name': 'Indy Jazz Festival', 'date': '2024-11-10', 'time': '6:30 PM', 'price': '\$35'},
+      {'name': 'Food Truck Roundup', 'date': '2024-11-14', 'time': '12:00 PM', 'price': 'Free'},
+      {'name': 'Gallery Opening', 'date': '2024-11-17', 'time': '5:00 PM', 'price': '\$20'}
+    ],
+    'Fort Worth, TX': [
+      {'name': 'Fort Worth Rodeo', 'date': '2024-11-09', 'time': '3:00 PM', 'price': '\$40'},
+      {'name': 'BBQ Fest', 'date': '2024-11-12', 'time': '12:00 PM', 'price': 'Free'},
+      {'name': 'Western Art Expo', 'date': '2024-11-16', 'time': '11:00 AM', 'price': '\$15'}
+    ],
+    'Charlotte, NC': [
+      {'name': 'Outdoor Music Jam', 'date': '2024-11-11', 'time': '6:00 PM', 'price': '\$25'},
+      {'name': 'Food & Art Fair', 'date': '2024-11-14', 'time': '10:00 AM', 'price': 'Free'},
+      {'name': 'Art in the Park', 'date': '2024-11-18', 'time': '9:00 AM', 'price': '\$20'}
+    ],
+    'San Francisco, CA': [
+      {'name': 'Golden Gate Jazz Night', 'date': '2024-11-10', 'time': '7:00 PM', 'price': '\$60'},
+      {'name': 'Gourmet Food Crawl', 'date': '2024-11-12', 'time': '1:00 PM', 'price': '\$45'},
+      {'name': 'Art & Wine Festival', 'date': '2024-11-15', 'time': '10:00 AM', 'price': '\$30'}
+    ],
+    'Seattle, WA': [
+      {'name': 'Music Under the Stars', 'date': '2024-11-09', 'time': '7:00 PM', 'price': '\$35'},
+      {'name': 'Seafood Festival', 'date': '2024-11-12', 'time': '11:00 AM', 'price': 'Free'},
+      {'name': 'Downtown Art Walk', 'date': '2024-11-17', 'time': '2:00 PM', 'price': '\$20'}
+    ],
+    'Denver, CO': [
+      {'name': 'Rocky Mountain Blues', 'date': '2024-11-11', 'time': '8:00 PM', 'price': '\$45'},
+      {'name': 'Food Fest', 'date': '2024-11-13', 'time': '3:00 PM', 'price': 'Free'},
+      {'name': 'Art Market', 'date': '2024-11-16', 'time': '10:00 AM', 'price': '\$15'}
+    ],
+    'Boston, MA': [
+      {'name': 'Back Bay Jazz Night', 'date': '2024-11-09', 'time': '6:30 PM', 'price': '\$50'},
+      {'name': 'Food Truck Gala', 'date': '2024-11-13', 'time': '12:00 PM', 'price': 'Free'},
+      {'name': 'Art at the Harbor', 'date': '2024-11-16', 'time': '10:00 AM', 'price': '\$25'}
+    ],
+    'Nashville, TN': [
+      {'name': 'Country Music Festival', 'date': '2024-11-10', 'time': '5:00 PM', 'price': '\$55'},
+      {'name': 'Southern Food Fest', 'date': '2024-11-12', 'time': '1:00 PM', 'price': 'Free'},
+      {'name': 'Art in Music City', 'date': '2024-11-15', 'time': '11:00 AM', 'price': '\$20'}
+    ],
+    'Detroit, MI': [
+      {'name': 'Detroit Jazz Fest', 'date': '2024-11-11', 'time': '7:00 PM', 'price': '\$40'},
+      {'name': 'Foodie Heaven', 'date': '2024-11-13', 'time': '11:00 AM', 'price': 'Free'},
+      {'name': 'Urban Art Showcase', 'date': '2024-11-16', 'time': '10:00 AM', 'price': '\$15'}
+    ],
+    'Oklahoma City, OK': [
+      {'name': 'OKC Rodeo Night', 'date': '2024-11-10', 'time': '4:00 PM', 'price': '\$20'},
+      {'name': 'Foodie Fest', 'date': '2024-11-14', 'time': '12:00 PM', 'price': 'Free'},
+      {'name': 'Art in the Park', 'date': '2024-11-18', 'time': '9:00 AM', 'price': '\$10'}
+    ],
+    'Portland, OR': [
+      {'name': 'Jazz in the Woods', 'date': '2024-11-10', 'time': '6:00 PM', 'price': '\$35'},
+      {'name': 'Gourmet Street Fare', 'date': '2024-11-13', 'time': '2:00 PM', 'price': 'Free'},
+      {'name': 'Art District Walk', 'date': '2024-11-15', 'time': '11:00 AM', 'price': '\$20'}
+    ],
+    'Las Vegas, NV': [
+      {'name': 'Vegas Night Concert', 'date': '2024-11-12', 'time': '8:00 PM', 'price': '\$50'},
+      {'name': 'Downtown Food Fest', 'date': '2024-11-14', 'time': '11:00 AM', 'price': 'Free'},
+      {'name': 'Street Art Experience', 'date': '2024-11-18', 'time': '2:00 PM', 'price': '\$30'}
+    ],
+    'Baltimore, MD': [
+      {'name': 'Harbor Jazz Night', 'date': '2024-11-10', 'time': '7:00 PM', 'price': '\$35'},
+      {'name': 'Crab Fest', 'date': '2024-11-12', 'time': '1:00 PM', 'price': 'Free'},
+      {'name': 'Waterfront Art Walk', 'date': '2024-11-17', 'time': '9:00 AM', 'price': '\$20'}
+    ],
+    'Milwaukee, WI': [
+      {'name': 'Lakefront Music Fest', 'date': '2024-11-11', 'time': '5:00 PM', 'price': '\$25'},
+      {'name': 'Food Truck Roundup', 'date': '2024-11-13', 'time': '12:00 PM', 'price': 'Free'},
+      {'name': 'Gallery Night', 'date': '2024-11-18', 'time': '6:00 PM', 'price': '\$15'}
+    ],
+    'Albuquerque, NM': [
+      {'name': 'Balloon Music Fest', 'date': '2024-11-10', 'time': '6:00 PM', 'price': '\$30'},
+      {'name': 'Street Food Fiesta', 'date': '2024-11-12', 'time': '12:00 PM', 'price': 'Free'},
+      {'name': 'Desert Art Walk', 'date': '2024-11-15', 'time': '10:00 AM', 'price': '\$20'}
+    ],
+    'Tucson, AZ': [
+      {'name': 'Desert Music Fest', 'date': '2024-11-11', 'time': '4:00 PM', 'price': '\$30'},
+      {'name': 'Food & Drink Fair', 'date': '2024-11-14', 'time': '12:00 PM', 'price': 'Free'},
+      {'name': 'Art Under the Stars', 'date': '2024-11-16', 'time': '9:00 AM', 'price': '\$20'}
+]
+
+
     // Add more events as needed
   };
 
